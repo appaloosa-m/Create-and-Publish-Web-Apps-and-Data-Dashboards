51509b631fb341964b7f893eadc922e7912caa16
Michael Simon <117365216+appaloosa-m@users.noreply.github.com>
2022-11-11 14:38:47 +0000

    Added Table

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-import streamlit as st 
+import streamlit as st
 import pandas
 
-
 data = {
-  'Series_1':[1, 3, 4, 5, 7],
+  'Series_1':[1, 3, 4, 6, 7],
   'Series 2':[10, 30, 40, 100, 250]
 }
 
 df = pandas.DataFrame(data)
 
-st.title('Our First Streamlit App')
+st.title('Our first Streamlit app')
 st.subheader('Introducing Streamlit in Automate Everything with Python')
-st.write('''This is our first Web App.
-Enjoy it!
-''')
+st.write('''This is our first Web App''')
 st.write(df)
-st.line_chart(df)
-st.area_chart(df)
-
-myslider = st.slider('Celsius')
-st.write(myslider, 'in Fahrenheit is', myslider * 9/5 + 32)
